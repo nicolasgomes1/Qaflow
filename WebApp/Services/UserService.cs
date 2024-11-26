445331c227b97eb2c2ecd55c0a5c1536249357cc
nicolasgomes1 <nicolasdiasgomes@gmail.com>
2024-11-26 23:41:59 +0000

    added some improvements to get the assigned to to store the username and not the guid

--- WebApp/Services/UserService.cs
+++ WebApp/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
+using WebApp.Data;
 
 namespace WebApp.Services;
 public class UserService(AuthenticationStateProvider authenticationStateProvider)
@@ -20,6 +21,19 @@ public class UserService(AuthenticationStateProvider authenticationStateProvider
     {
         var userInfo = await GetCurrentUserInfoAsync();
         return userInfo.UserName;
+    }
+    
+    
+    /// <summary>
+    /// Returns the name of the user with the given id. To be used in the dropdowns
+    /// </summary>
+    /// <param name="userId"> The selected User in the dropdown with its guid</param>
+    /// <param name="userList">The List of Users </param>
+    /// <returns></returns>
+    public static string GetUserName(string userId, List<ApplicationUser> userList)
+    {
+        var user = userList.FirstOrDefault(user => user.Id == userId);
+        return user?.UserName ?? "Unassigned";
     }
 }
 
