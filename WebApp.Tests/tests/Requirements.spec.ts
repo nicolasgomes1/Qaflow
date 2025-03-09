80df52ca0f5eded0b7b17e51de34b316a0494b40
nicolasgomes1 <109753002+nicolasgomes1@users.noreply.github.com>
2025-03-09 22:09:06 +0100

    add more tests

--- WebApp.Tests/tests/Requirements.spec.ts
+++ WebApp.Tests/tests/Requirements.spec.ts
@@ -21,7 +21,6 @@ async function filterAndLaunchProject(page: Page, projectName = 'Demo Project Wi
     await page.getByRole('textbox', { name: 'Name filter value' }).click();
     await page.getByRole('textbox', { name: 'Name filter value' }).fill(projectName);
     await page.getByRole('button', { name: 'Apply' }).click();
-
     await page.getByRole('button', { name: 'launch' }).first().click({ force: true });
     await page.waitForLoadState('load');
 }
@@ -65,4 +64,49 @@ test('Create New Requirement', async ({ page })=> {
     await page.getByRole('button', { name: 'delete', exact: true }).click();
     await page.getByRole('button', { name: 'Ok' }).click();
     await expect(page.getByRole('table')).not.toContainText('Test Requirement Playwright' + Random);
+});
+
+test('View New Requirement', async ({ page })=> {
+    test.slow();
+    const Random = Math.floor(Math.random() * 1000);
+
+    await filterAndLaunchProject(page);
+
+    await actions.click_button(page, 'd_requirements');
+
+    await actions.click_button(page, 'create_requirement');
+
+    const name = 'Test Requirement Playwright' + Random;
+    await actions.fill_input(page, 'requirement_name', name);
+
+    const description = 'Test Requirement Playwright Description' + Random;
+    await actions.fill_input(page, 'requirement_description', description);
+
+    await actions.select_dropdown_option(page, 'requirement_priority', 'Medium');
+
+
+    await actions.select_dropdown_option(page,'requirement_status', 'Completed');
+
+    await actions.select_dropdown_option(page,'requirement_assignedto', 'user@example.com');
+
+    await actions.submit_form(page);
+
+    await actions.validate_button(page, 'create_requirement');
+
+
+    //view the created requirement
+    await page.getByRole('columnheader', { name: 'Name sort   filter_alt' }).locator('i').click();
+    await page.getByRole('textbox', { name: 'Name filter value' }).click();
+    await page.getByRole('textbox', { name: 'Name filter value' }).fill(name);
+    await page.getByRole('button', { name: 'Apply' }).click();
+    if (await page.locator('.rz-notification-item > div:nth-child(2)').isVisible()) {
+        await page.locator('.rz-notification-item > div:nth-child(2)').click();
+    }
+    await expect(page.getByRole('table')).toContainText(name);
+
+    await actions.click_button(page, 'view');
+
+    expect(page.getByText(name)).toBeVisible();
+    expect(page.getByText(description)).toBeVisible();
+
 });
