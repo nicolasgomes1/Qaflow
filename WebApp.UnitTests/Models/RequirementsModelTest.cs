5be101afe8e93f7a38311b9e5e942fc8b990f736
nicolasgomes1 <109753002+nicolasgomes1@users.noreply.github.com>
2025-04-07 21:33:58 +0100

    Add unit tests for updating requirements with specifications/files
    This commit expands `RequirementsModelTest` with tests for handling requirement updates involving specifications and file uploads. It includes scenarios for adding, updating, and retrieving files, as well as validating specification and status updates. Added `TestBrowserFile` to simulate browser file uploads in tests.

--- WebApp.UnitTests/Models/RequirementsModelTest.cs
+++ WebApp.UnitTests/Models/RequirementsModelTest.cs
@@ -1,6 +1,8 @@
 using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
+using WebApp.Data.enums;
 using WebApp.Models;
 using WebApp.Services;
 using WebApp.UnitTests.BaseTest;
@@ -14,6 +16,7 @@ public class RequirementsModelTest : IClassFixture<TestFixture>
     private readonly ApplicationDbContext db;
     private readonly RequirementsModel rm;
     private readonly ProjectModel pm;
+    private readonly RequirementsFilesModel rfm;
 
     public RequirementsModelTest(TestFixture fixture)
     {
@@ -21,6 +24,7 @@ public class RequirementsModelTest : IClassFixture<TestFixture>
         db = fixture.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         rm = fixture.ServiceProvider.GetRequiredService<RequirementsModel>();
         pm = fixture.ServiceProvider.GetRequiredService<ProjectModel>();
+        rfm = fixture.ServiceProvider.GetRequiredService<RequirementsFilesModel>();
     }
 
     private async Task<int> GetRequirementCountAsync()
@@ -79,12 +83,219 @@ public class RequirementsModelTest : IClassFixture<TestFixture>
         newRequirement.Name = "updated";
         newRequirement.Description = "updated description";
 
+        newRequirement.Priority = Priority.Critical;
+        newRequirement.AssignedTo = "admin@example.com";
+        newRequirement.WorkflowStatus = WorkflowStatus.New;
+        newRequirement.ArchivedStatus = ArchivedStatus.Active;
+
         await rm.UpdateRequirement(newRequirement, null, project.Id);
-       // db.Requirements.Update(newRequirement);
+        // db.Requirements.Update(newRequirement);
         //await db.SaveChangesAsync();
 
         var updatedRequirement = await rm.GetRequirementByIdAsync(newRequirement.Id);
         Assert.Equal("updated", updatedRequirement.Name);
         Assert.Equal("updated description", updatedRequirement.Description);
+        Assert.Equal(Priority.Critical, updatedRequirement.Priority);
+        Assert.Equal("admin@example.com", updatedRequirement.AssignedTo);
+        Assert.Equal(WorkflowStatus.New, updatedRequirement.WorkflowStatus);
+        Assert.Equal(ArchivedStatus.Active, updatedRequirement.ArchivedStatus);
+    }
+
+    [Fact]
+    public async Task RequirementsModel_UpdateRequirementWithRequirementsSpecifications()
+    {
+        var project = await CreateAndSetProject();
+
+        var newRequirement = new Requirements
+        {
+            Name = "original",
+            Description = "original description",
+            ProjectsId = project.Id
+        };
+
+        await rm.AddRequirement(newRequirement, null, project.Id);
+
+        var requirementspec = await db.RequirementsSpecification.Where(x => x.Name == "Requirements Specification 3")
+            .FirstOrDefaultAsync();
+
+        if (requirementspec == null) throw new Exception("Requirements Specification 3 not found");
+        newRequirement.Name = "updated";
+        newRequirement.Description = "updated description";
+
+        newRequirement.Priority = Priority.Critical;
+        newRequirement.AssignedTo = "admin@example.com";
+        newRequirement.WorkflowStatus = WorkflowStatus.New;
+        newRequirement.ArchivedStatus = ArchivedStatus.Active;
+        newRequirement.RequirementsSpecificationId = requirementspec.Id;
+
+        await rm.UpdateRequirement(newRequirement, null, project.Id);
+
+
+        var updatedRequirement = await rm.GetRequirementByIdAsync(newRequirement.Id);
+        Assert.Equal("updated", updatedRequirement.Name);
+        Assert.Equal("updated description", updatedRequirement.Description);
+        Assert.Equal(Priority.Critical, updatedRequirement.Priority);
+        Assert.Equal("admin@example.com", updatedRequirement.AssignedTo);
+        Assert.Equal(WorkflowStatus.New, updatedRequirement.WorkflowStatus);
+        Assert.Equal(ArchivedStatus.Active, updatedRequirement.ArchivedStatus);
+        Assert.Equal(requirementspec.Id, updatedRequirement.RequirementsSpecificationId);
+    }
+
+    [Fact]
+    public async Task RequirementsModel_UpdateRequirementWithRequirementsSpecificationsDelete()
+    {
+        var project = await CreateAndSetProject();
+        var requirementSpec = await db.RequirementsSpecification.Where(x => x.Name == "Requirements Specification 3")
+            .FirstOrDefaultAsync();
+        if (requirementSpec == null) throw new Exception("Requirements Specification 3 not found");
+
+        var newRequirement = new Requirements
+        {
+            Name = "original",
+            Description = "original description",
+            ProjectsId = project.Id,
+            RequirementsSpecificationId = requirementSpec.Id // Requirements Specification 1 is deleted in the database
+        };
+
+        await rm.AddRequirement(newRequirement, null, project.Id);
+
+
+        newRequirement.Name = "updated";
+        newRequirement.Description = "updated description";
+
+        newRequirement.Priority = Priority.Critical;
+        newRequirement.AssignedTo = "admin@example.com";
+        newRequirement.WorkflowStatus = WorkflowStatus.New;
+        newRequirement.ArchivedStatus = ArchivedStatus.Active;
+        newRequirement.RequirementsSpecificationId = -1;
+
+        await rm.UpdateRequirement(newRequirement, null, project.Id);
+
+
+        var updatedRequirement = await rm.GetRequirementByIdAsync(newRequirement.Id);
+        Assert.Equal("updated", updatedRequirement.Name);
+        Assert.Equal("updated description", updatedRequirement.Description);
+        Assert.Equal(Priority.Critical, updatedRequirement.Priority);
+        Assert.Equal("admin@example.com", updatedRequirement.AssignedTo);
+        Assert.Equal(WorkflowStatus.New, updatedRequirement.WorkflowStatus);
+        Assert.Equal(ArchivedStatus.Active, updatedRequirement.ArchivedStatus);
+        Assert.Equal(-1,
+            db.Requirements.Where(x => x.Id == updatedRequirement.Id).Select(x => x.RequirementsSpecificationId)
+                .FirstOrDefault());
+        Assert.DoesNotContain(db.Requirements.Where(x => x.Id == updatedRequirement.Id),
+            x => x.RequirementsSpecification != null);
+    }
+
+    [Fact]
+    public async Task RequirementsModel_AddRequirementWithFiles()
+    {
+        var project = await CreateAndSetProject();
+
+        var newRequirement = new Requirements
+        {
+            Name = "original",
+            Description = "original description",
+            ProjectsId = project.Id
+        };
+
+        var bytes = new byte[] { 115, 116, 114, 105, 103, 110 };
+
+        var browserFiles = new List<IBrowserFile>
+        {
+            new TestBrowserFile("test.txt", bytes),
+            new TestBrowserFile("test1.txt", bytes)
+        };
+
+        await rm.AddRequirement(newRequirement, browserFiles, project.Id);
+
+        newRequirement.Name = "updated";
+        newRequirement.Description = "updated description";
+        newRequirement.Priority = Priority.Critical;
+        newRequirement.AssignedTo = "admin@example.com";
+        newRequirement.WorkflowStatus = WorkflowStatus.New;
+        newRequirement.ArchivedStatus = ArchivedStatus.Active;
+
+        await rm.UpdateRequirement(newRequirement, null, project.Id);
+
+
+        var requirementFiles = db.RequirementsFiles.Where(x => x.RequirementsId == newRequirement.Id).ToList();
+
+        Assert.Equal(2, requirementFiles.Count);
+        var updatedRequirement = await rm.GetRequirementByIdAsync(newRequirement.Id);
+    }
+
+    [Fact]
+    public async Task RequirementsModel_AddRequirementWithFilesUpdateFiles()
+    {
+        var project = await CreateAndSetProject();
+
+        var newRequirement = new Requirements
+        {
+            Name = "original",
+            Description = "original description",
+            ProjectsId = project.Id
+        };
+
+        var bytes = new byte[] { 115, 116, 114, 105, 103, 110 };
+
+        var browserFiles = new List<IBrowserFile>
+        {
+            new TestBrowserFile("test.txt", bytes),
+            new TestBrowserFile("test1.txt", bytes)
+        };
+
+        await rfm.SaveFilesToDb(browserFiles, newRequirement.Id, project.Id);
+        await rm.AddRequirement(newRequirement, browserFiles, project.Id);
+    }
+
+    [Fact]
+    public async Task RequirementsModel_GetRequirementsFiles()
+    {
+        var project = await CreateAndSetProject();
+
+        var newRequirement = new Requirements
+        {
+            Name = "original",
+            Description = "original description",
+            ProjectsId = project.Id
+        };
+
+        var bytes = new byte[] { 115, 116, 114, 105, 103, 110 };
+
+        var browserFiles = new List<IBrowserFile>
+        {
+            new TestBrowserFile("test.txt", bytes),
+            new TestBrowserFile("test1.txt", bytes)
+        };
+
+        await rfm.SaveFilesToDb(browserFiles, newRequirement.Id, project.Id);
+        await rm.AddRequirement(newRequirement, browserFiles, project.Id);
+
+        var files = await rfm.GetFilesByRequirementId(newRequirement.Id);
+        Assert.Equal(2, files.Count);
+    }
+
+
+    private class TestBrowserFile : IBrowserFile
+    {
+        public TestBrowserFile(string name, byte[] content)
+        {
+            Name = name;
+            LastModified = DateTimeOffset.Now;
+            ContentType = "text/plain";
+            Size = content.Length;
+            Content = content;
+        }
+
+        public string Name { get; }
+        public DateTimeOffset LastModified { get; }
+        public string ContentType { get; }
+        public long Size { get; }
+        public byte[] Content { get; }
+
+        public Stream OpenReadStream(long maxAllowedSize = 512000, CancellationToken cancellationToken = default)
+        {
+            return new MemoryStream(Content);
+        }
     }
 }
