9c176fd45a39cd34e3b5c44acd596f05dc779bae
nicol <nicolasdiasgomes@gmail.com>
2025-08-14 17:00:57 +0200

    Refactored workflow card rendering logic into reusable service `WkfManagementCardColor`. Updated related components to use the new implementation.

---
+++ WebApp/Services/WkfManagementCardColor.cs
@@ -0,0 +1,27 @@
+using Radzen;
+using WebApp.Data;
+using WebApp.Data.enums;
+
+namespace WebApp.Services;
+
+public static class WkfManagementCardColor
+{
+    public static void OnItemRender<T>(RadzenDropZoneItemRenderEventArgs<T> args)
+    {
+        var workflowStatus = (WorkflowStatus)(typeof(T)
+            .GetProperty("WorkflowStatus")?
+            .GetValue(args.Item) ?? null!);
+
+        args.Attributes["class"] =
+            "rz-card rz-variant-filled rz-background-color-primary-light rz-color-on-primary-light";
+
+        args.Attributes["class"] += workflowStatus switch
+        {
+            WorkflowStatus.New => " rz-border-danger",
+            WorkflowStatus.InReview => " rz-border-warning",
+            WorkflowStatus.Completed => " rz-border-success",
+            WorkflowStatus.Reopened => " rz-border-primary",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
