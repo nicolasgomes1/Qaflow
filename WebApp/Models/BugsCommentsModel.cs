bf6dbce859e378c30ab664093481c853e78a31ea
nicol <nicolasdiasgomes@gmail.com>
2025-08-17 23:22:27 +0200

    zerzer

--- WebApp/Models/BugsCommentsModel.cs
+++ WebApp/Models/BugsCommentsModel.cs
@@ -24,4 +24,27 @@ public class BugsCommentsModel(IDbContextFactory<ApplicationDbContext> dbContext
         await db.BugsComments.AddAsync(bugsComment);
         await db.SaveChangesAsync();
     }
+
+    public async Task DeleteBugComment(int bugCommentId)
+    {
+        await using var db = await dbContextFactory.CreateDbContextAsync();
+        var bugComment = await db.BugsComments.FindAsync(bugCommentId);
+        if (bugComment is null) throw new Exception("Bug Comment not found");
+        db.BugsComments.Remove(bugComment);
+        await db.SaveChangesAsync();
+    }
+
+    public async Task UpdateBugComment(int bugCommentId, BugsComments bugsComment)
+    {
+        await using var db = await dbContextFactory.CreateDbContextAsync();
+        var bugCommentDb = await db.BugsComments.FindAsync(bugCommentId);
+        if (bugCommentDb is null) throw new Exception("Bug Comment not found");
+
+        bugCommentDb.Comment = bugsComment.Comment;
+        bugCommentDb.ModifiedAt = DateTime.UtcNow;
+        bugCommentDb.ModifiedBy = (await userService.GetCurrentUserInfoAsync()).UserName;
+
+        db.BugsComments.Update(bugCommentDb);
+        await db.SaveChangesAsync(); // <--- missing!
+    }
 }
