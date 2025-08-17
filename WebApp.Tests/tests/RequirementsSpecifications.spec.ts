b45f6b30db9ca8f3781c5a4ab9b2a4871b6ee3a3
nicol <nicolasdiasgomes@gmail.com>
2025-08-17 14:32:37 +0200

    fdfd

--- WebApp.Tests/tests/RequirementsSpecifications.spec.ts
+++ WebApp.Tests/tests/RequirementsSpecifications.spec.ts
@@ -116,4 +116,40 @@ test('Delete Requirement Specification', async ({ page })=> {
     await filter.filterTableModel(page, name);
 
     await actions.click_button(page, 'delete');
+
+    // Ensure the dialog is visible
+    const dialog = page.locator('.rz-dialog');
+
+    // Check if the dialog is visible
+    await expect(dialog).toBeVisible();
+
+    await page.getByRole('button', { name: 'Ok' }).first().click();
+    await expect(page.getByRole('table')).not.toContainText('Test Requirement Playwright' + Random);
+})
+
+test('Delete Requirement Specification is not possible with linked requirement', async ({ page })=> {
+
+    test.slow();
+
+    await actions.LaunchProject(page, 'Demo Project With Data' );
+
+    await actions.click_button(page, 'd_requirementsSpecification');
+
+    await page.waitForLoadState('networkidle')
+    const name = 'Requirements Specification 1';
+
+
+    await filter.filterTableModel(page, name);
+
+    await actions.click_button(page, 'delete');
+
+    // Ensure the dialog is visible
+    const dialog = page.locator('.rz-dialog');
+
+    // Check if the dialog is visible
+    await expect(dialog).toBeVisible();
+
+    await page.getByRole('button', { name: 'Ok' }).first().click();
+
+    await expect(page.locator('.rz-notification-item').getByText("Can't delete Requirements Specification has it has children")).toBeVisible();
 })
